05f2976b26bc56c15c145a8cf9faa4529c658b4f
wzaiz <82254937+wzaiz@users.noreply.github.com>
2023-12-03 21:20:31 +0800

    Update __xmltv.py

--- epg/scraper/__xmltv.py
+++ epg/scraper/__xmltv.py
@@ -5,8 +5,7 @@ from datetime import datetime
 from io import BytesIO
 from . import headers
 from epg.scraper import tz_shanghai
-
-xmltv_url = 'https://epg.112114.xyz/pp.xml'
+export xmltv_url = 'https://epg.112114.xyz/pp.xml'
 
 def get_channels(xmltv_url: str, dtd: etree.DTD | None = None) -> list[Channel]:
     try:
