6230de35e53abc5aaf1e96176bc425c7ea2c7697
wzaiz <82254937+wzaiz@users.noreply.github.com>
2023-12-03 21:27:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from datetime import datetime, timezone
 from croniter import croniter
 import os
 import shutil
+os.environ['xmltv_url'] = 'https://epg.112114.xyz/pp.xml'
 
 CF_PAGES = os.getenv('CF_PAGES')
 CF_PAGES_URL = os.getenv('CF_PAGES_URL')
